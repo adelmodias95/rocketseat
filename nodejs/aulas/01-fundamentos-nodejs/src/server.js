fae3ccbcbb009bda35b102174944be63c060095b
adelmodias95 <adelmo.dias@mzgroup.com>
2024-08-21 15:42:50 -0300

    studies

--- nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,10 +1,45 @@
 // CommonJS => require .... const http = require("http");
 // ESModules => import/export .... import http from "http";
-
 import http from "node:http";
 
+// Stateful - Salva o estado da aplicação em memória até que a aplicacão seja reiniciada
+// Stateless - Não salva nada em memória, a aplicação vai guardar as informações em um banco de dados.
+
+// JSON - Javascript Object Notation
+
+// Cabeçalho (Requisicão/Resposta) => Metadados - Informações adicionais
+
+// HTTP Status Code => 200, 201, 400, 404, 500
+
+const users = [];
+
 const server = http.createServer((request, response) => {
-    return response.end("Hello Ignite!");
+    /*
+        Requisições HTTP
+        - Método HTTP
+        - URL
+
+        Métodos: GET, POST, PUT, PATCH, DELETE
+
+        GET => Buscar uma informação
+        POST => Inserir (Criar) uma informação
+        PUT => Alterar uma informaçãos
+        PATCH => Alterar uma informação específica
+        DELETE => Deletar uma informação
+    */
+    const { method, url } = request;
+
+    if (method == "GET" && url == "/users") {
+        return response.setHeader("Content-Type", "application/json").end(JSON.stringify(users));
+    }
+
+    if (method == "POST" && url == "/users") {
+        users.push({ id: 1, name: "Adelmo", email: "contato@adelmodias.com.br" });
+
+        return response.writeHead(201).end();
+    }
+
+    return response.writeHead(404).end("Not Found");
 });
 
 server.listen(3333, () => {
